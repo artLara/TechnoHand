45d84aeb065a5f701f00a7cc649e323983d98d75
artLara <art.lara.caz@gmail.com>
2022-10-16 10:04:35 -0500

    E-mail and password text field developed with material design

--- app/src/main/java/matur/technohand/LogIn.kt
+++ app/src/main/java/matur/technohand/LogIn.kt
@@ -10,11 +10,11 @@ class LogIn : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
         supportActionBar?.hide()
-        val buttonSingUp = findViewById<Button>(R.id.sing_up_button)
-        buttonSingUp.setOnClickListener {
-            val intent = Intent(this, SingUp::class.java)
-            startActivity(intent)
-        }
+//        val buttonSingUp = findViewById<Button>(R.id.sing_up_button)
+//        buttonSingUp.setOnClickListener {
+//            val intent = Intent(this, SingUp::class.java)
+//            startActivity(intent)
+//        }
 
     }
 }
