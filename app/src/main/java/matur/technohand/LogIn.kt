88ddf1d07f1531bf20956b0a5024044daf9b7f03
artLara <art.lara.caz@gmail.com>
2022-10-16 18:00:55 -0500

    Sign up fragment

--- app/src/main/java/matur/technohand/LogIn.kt
+++ app/src/main/java/matur/technohand/LogIn.kt
@@ -10,11 +10,18 @@ class LogIn : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
         supportActionBar?.hide()
-//        val buttonSingUp = findViewById<Button>(R.id.sing_up_button)
-//        buttonSingUp.setOnClickListener {
-//            val intent = Intent(this, SingUp::class.java)
-//            startActivity(intent)
-//        }
+        val buttonSingUp = findViewById<Button>(R.id.sing_up_button)
+        buttonSingUp.setOnClickListener {
+//          With activity
+//          val intent = Intent(this, SingUp::class.java)
+//          startActivity(intent)
+
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+//            var fragment = SignUpFragment()
+            fragmentTransaction.add(R.id.login_layout, SignUpFragment())
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
 
     }
 }
